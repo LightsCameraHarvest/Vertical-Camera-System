b35c0d959eedd64039e1ead850b2f265bdeb76e7
helenaw041 <helena.w041@gmail.com>
2025-07-17 18:10:58 -0500

    merging

--- controls.py
+++ controls.py
@@ -1,7 +1,7 @@
-# Located in raspberry pi to control motors. 
+# Raspberry Pi file path: /home/helena/controls.py
 
+import pigpio
 import RPi.GPIO as GPIO
-from gpiozero import AngularServo
 from time import sleep
 
 # Pin definitions
@@ -9,22 +9,24 @@ ena_pin = 17
 dir_pin = 27
 stp_pin = 22
 motor_step_size = 200
-max_steps = 1000 # ***NEED TO TEST!!!***
+max_steps = 1000  # ***NEED TO TEST!!!***
 
-servo_pin = 13 # or 19
-myServo = AngularServo(servo_pin, min_angle=0, max_angle=270)
-min_angle = 0
-max_angle = 270
-servo_pos = max_angle // 2
-servo_step_size = 5
-myServo.angle = servo_pos
-
-limit_pin = 6 # or 26
-# because we have no bottom limit pin, we need to keep track of steps 
+servo_pin = 13  # or 19
+min_pulse = 1000
+max_pulse = 2000
+servo_pos = max_pulse // 2
+servo_step_size = 20
+limit_pin = 26  # or 26
+# because we have no bottom limit pin, we need to keep track of steps
 total_steps = 0
 
+pi = pigpio.pi()
+if not pi.connected:
+    print("Failed to connect to pigpiod. Is it running?")
+    exit(1)
+
 # GPIO setup
-GPIO.setmode(GPIO.BCM) # GPIO numbers not board numbers
+GPIO.setmode(GPIO.BCM)  # GPIO numbers not board numbers
 GPIO.setup(ena_pin, GPIO.OUT)
 GPIO.setup(dir_pin, GPIO.OUT)
 GPIO.setup(stp_pin, GPIO.OUT)
@@ -33,6 +35,7 @@ GPIO.setup(limit_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 current_level = 0
 target_level = 0
 
+pi.set_servo_pulsewidth(servo_pin, servo_pos)
 
 def isTopPressed():
     return GPIO.input(limit_pin) == GPIO.LOW
@@ -41,7 +44,7 @@ def goHome():
     global current_level, target_level
     GPIO.output(ena_pin, GPIO.LOW)
     print("Going home...")
-  
+
     while not isTopPressed():
         # step pulse
         GPIO.output(stp_pin, GPIO.HIGH)
@@ -57,36 +60,38 @@ def goHome():
     return
 
 def stepUp():
-  global total_steps
-  if not isTopPressed():
-    for i in range(motor_step_size):
+    global total_steps
+    if not isTopPressed():
         GPIO.output(dir_pin, GPIO.HIGH)
-        GPIO.output(stp_pin, GPIO.HIGH)
         sleep(0.001)
-        GPIO.output(stp_pin, GPIO.LOW)
-        sleep(0.001)
-        total_steps += 1
+        for i in range(motor_step_size):
+            GPIO.output(stp_pin, GPIO.HIGH)
+            sleep(0.001)
+            GPIO.output(stp_pin, GPIO.LOW)
+            sleep(0.001)
+            total_steps += 1
 
 def stepDown():
-  global total_steps
-  if total_steps <= max_steps:
-    for i in range(motor_step_size):
+    global total_steps
+    if total_steps <= max_steps:
         GPIO.output(dir_pin, GPIO.LOW)
-        GPIO.output(stp_pin, GPIO.HIGH)
-        sleep(1)
-        GPIO.output(stp_pin, GPIO.LOW)
-        sleep(1)
-        total_steps -= 1
+        sleep(0.001)
+        for i in range(motor_step_size):
+            GPIO.output(stp_pin, GPIO.HIGH)
+            sleep(0.001)
+            GPIO.output(stp_pin, GPIO.LOW)
+            sleep(0.001)
+            total_steps -= 1
 
 def panCCW():
     global servo_pos
-    servo_pos = max(min_angle, servo_pos - servo_step_size)
-    myServo.angle = servo_pos
+    servo_pos = max(min_pulse, servo_pos - servo_step_size)
+    pi.set_servo_pulsewidth(servo_pin, servo_pos)
 
 def panCW():
     global servo_pos
-    servo_pos = min(max_angle, servo_pos + servo_step_size)
-    myServo.angle = servo_pos
+    servo_pos = min(max_pulse, servo_pos + servo_step_size)
+    pi.set_servo_pulsewidth(servo_pin, servo_pos)
 
 def getCommand():
     try:
@@ -113,7 +118,12 @@ def mainLoop():
     except KeyboardInterrupt:
         print("Exiting...")
     finally:
-        GPIO.cleanup()
+        if pi.connected:
+            try:
+                pi.set_servo_pulsewidth(servo_pin, 0)
+                pi.stop()
+            except Exception as e:
+                print(f"Error during pigpio cleanup: {e}")
 
 if __name__ == "__main__":
     mainLoop()
